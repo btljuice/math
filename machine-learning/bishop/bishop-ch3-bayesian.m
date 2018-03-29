96cc91922a9f68df5033494a6483b8d6c240f99a
btljuice <alexis.trudeau@gmail.com>
2018-03-28 22:13:37 -0400

    bishop ch.3 bayesian: Finished figure 3.7.

--- machine-learning/bishop/bishop-ch3-bayesian.m
+++ machine-learning/bishop/bishop-ch3-bayesian.m
@@ -15,8 +15,25 @@ function r = f_th(x)
   r = [ x0 x ]*w_th;
 endfunction
 
+function r = sample_param_pdf(mu, sigma, n)
+  r = mvnrnd(mu', sigma, n);
+endfunction
+
+function r = sample_prior_pdf(n)
+  global m_0;
+  global S_0;
+
+  r = sample_param_pdf(m_0, S_0, n);
+endfunction
+
+function r = sample_posterior_pdf(n, t, x)
+  [m_n, S_n] = posterior_mean_and_variance(t, x);
+
+  r = sample_param_pdf(m_n, S_n, n);
+endfunction
+
 function r = param_pdf(w0, w1, mu, sigma)
-  assert(size_equal(w0, w1) && issquare(sigma) )
+  assert(size_equal(w0, w1) && issquare(sigma) );
 
   w = [ w0'(:) w1'(:) ];  # Make it a n x 2 matrix to fit mvnpdf arguments
   r = mvnpdf(w, mu', sigma);
@@ -30,16 +47,21 @@ function r = prior_pdf(w0, w1)
   r = param_pdf(w0, w1, m_0, S_0)
 endfunction
 
-function r = posterior_pdf(w0, w1, t, x)
+function [m_n, S_n] = posterior_mean_and_variance(t, x)
   global m_0;
   global S_0;
   global beta_param;
-  assert(size_equal(w0, w1) && isvector(t));
+  assert(isvector(t) && size(x,1) == length(t));
 
   Phi = [ ones(length(x), 1) x];
   S_n = (S_0^-1 + beta_param*Phi'*Phi)^-1;
   m_n = S_n*(S_0^-1*m_0 + beta_param*Phi'*t);
+endfunction
 
+function r = posterior_pdf(w0, w1, t, x)
+  assert(size_equal(w0, w1) && isvector(t));
+
+  [m_n, S_n] = posterior_mean_and_variance(t, x);
   r = param_pdf(w0, w1, m_n, S_n);
 endfunction
 
@@ -58,31 +80,7 @@ function ll = likelihood_pdf(t, x, w0, w1)
   end
 endfunction
 
-function plot_likelihood_pdf(t, x)
-  global w_th;
-  range = -1:.001:1;
-  [w0, w1] = meshgrid(range, range);
-  p = likelihood_pdf(t, x, w0, w1);
-  colormap(jet)
-  hold on;
-  contourf(w0, w1, p, 200, 'LineStyle', 'None')
-  scatter(w_th(1), w_th(2), 50, 'w', 'o', 'filled')
-  hold off;
-endfunction
-
-function plot_param_pdf(m_n, S_n)
-  global w_th;
-  range = -1:.05:1;
-  [w0, w1] = meshgrid(range, range);
-  p = param_pdf(w0, w1, m_n, S_n);
-  colormap(jet)
-          hold on;
-  contourf(w0, w1, p, 200, 'LineStyle', 'None')
-          scatter(w_th(1), w_th(2), 50, 'w', 'o', 'filled')
-          hold off;
-endfunction
-
-function plot_pdf(pdf)
+function contour_pdf(pdf)
   global w_th;
   range = -1:.05:1;
   [w0, w1] = meshgrid(range, range);
@@ -94,42 +92,52 @@ function plot_pdf(pdf)
   hold off;
 endfunction
 
-function plot_prior_pdf()
+function plot_2dlines(w, pts_t, pts_x)
   global w_th;
-  range = -1:.05:1;
-  [w0, w1] = meshgrid(range, range);
-  p = prior_pdf(w0, w1);
-  colormap(jet)
+
+  n = size(w, 1);
+  x = -1:.05:1;
   hold on;
-  contourf(w0, w1, p, 200, 'LineStyle', 'None')
-  scatter(w_th(1), w_th(2), 50, 'w', 'o', 'filled')
+  for i = 1:size(w, 1)
+    plot(x, w(i, 1) + w(i, 2).*x, '-r')
+  end
+  plot(x, w_th(1) + w_th(2)*x, '-g')
+  if nargin == 3
+    scatter(pts_x, pts_t, 'b', 'o')
+  endif
+  xlim([-1, 1])
+  ylim([-1, 1])
   hold off;
 endfunction
 
-function plot_posterior_pdf(t, x)
-  global w_th;
-  range = -1:.05:1;
-  [w0, w1] = meshgrid(range, range);
-  p = posterior_pdf(w0, w1, t, x);
-  colormap(jet)
-  hold on;
-  contourf(w0, w1, p, 200, 'LineStyle', 'None')
-  scatter(w_th(1), w_th(2), 50, 'w', 'o', 'filled')
-  hold off;
-endfunction
-
+x = rand([20 1]);
+t = f_th(x) + normrnd(0, beta_param^-1, [20 1] );
+x = [1; -.6; x ];
+t = [0; -.75; t ];
 clf;
 subplot(4, 3, 1)
 title('likelihood')
 subplot(4, 3, 2)
 title('prior/posterior')
-plot_prior_pdf()
+contour_pdf(@prior_pdf)
+subplot(4, 3, 3)
+title('data space')
+plot_2dlines(sample_prior_pdf(5))
 subplot(4, 3, 4)
-plot_likelihood_pdf([0], [1])
+contour_pdf(@(w0, w1) likelihood_pdf(t(1), x(1), w0, w1))
 subplot(4, 3, 5)
-plot_posterior_pdf([0], [1])
+contour_pdf(@(w0, w1) posterior_pdf(w0, w1, t(1), x(1)))
+subplot(4, 3, 6)
+plot_2dlines(sample_posterior_pdf(5, t(1), x(1)), t(1), x(1))
 subplot(4, 3, 7)
-plot_likelihood_pdf([-.75], [-.6])
+contour_pdf(@(w0, w1) likelihood_pdf(t(2), x(2), w0, w1))
 subplot(4, 3, 8)
-plot_posterior_pdf([0; -.75], [1; -.6])
-
+contour_pdf(@(w0, w1) posterior_pdf(w0, w1, t(1:2), x(1:2)))
+subplot(4, 3, 9)
+plot_2dlines(sample_posterior_pdf(5, t(1:2), x(1:2)), t(1:2), x(1:2))
+subplot(4, 3, 10)
+contour_pdf(@(w0, w1) likelihood_pdf(t, x, w0, w1))
+subplot(4, 3, 11)
+contour_pdf(@(w0, w1) posterior_pdf(w0, w1, t, x))
+subplot(4, 3, 12)
+plot_2dlines(sample_posterior_pdf(5, t, x), t, x)
